2ca57bc274058b31d328b5aedaf2b8a1a4735381
Vinh Ta <Vinh Ta>
2021-03-13 16:16:22 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 3.32.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.59.0"
     }
   }
 }
@@ -18,6 +22,11 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+provider "google" {
+  project     = "cloud-infra-306917"
+  region      = "europe-west2"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0b6b51e397faf2316"
   instance_type = "t2.micro"
@@ -25,4 +34,12 @@ resource "aws_instance" "example" {
   tags = {
     Name = "ExampleInstance"
   }
+}
+
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store-vinh1234"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
 }
